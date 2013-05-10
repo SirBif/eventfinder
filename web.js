acec4404456047678a3372cb80e24206ac2c7ade
SirBif <bifmadeinsabbioni@gmail.com>
2013-05-10 22:58:33 +0200

    replaced everything

--- web.js
+++ web.js
@@ -1,13 +1,20 @@
 var express = require('express');
-var Facebook = require('facebook-node-sdk');
+var Facebook = require('facebook-node-sdk');var async   = require('async');
+var util    = require('util');
 
-var app = express.createServer();
+var app = express.createServer(express.logger());
 
 app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'sdjdkssdm8sdf89fmdf8sdfmsd' }));
   app.use(Facebook.middleware({ appId: '180798855409162', secret: '7d5b6a35d8c08d6aac512b6d85243a2f' }));
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
 
 app.get('/', Facebook.loginRequired(), function (req, res) {
