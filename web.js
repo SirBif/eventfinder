7fcb4e46286fd719cd7a90ff13b2e41c598739fa
SirBif <bifmadeinsabbioni@gmail.com>
2013-05-12 17:49:24 +0200

    tentative server retrieve

--- web.js
+++ web.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
 
 app.get('/doAnUpdate', Facebook.loginRequired(), function (req, res) {
 	var query = "/fql?q=" + escape("SELECT eid, start_time FROM event WHERE privacy='OPEN' AND start_time > now() AND eid IN (SELECT eid FROM event_member WHERE start_time > now() AND (uid IN(SELECT uid2 FROM friend WHERE uid1=me()) OR uid=me())ORDER BY start_time ASC LIMIT 50) ORDER BY start_time ASC ");
-	req.facebook.api(query, function(err, result) {
+	req.facebook.api('/me', function(err, result) {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 		console.log(result.data.length);
 		res.end('Results: ' + result.data.length);
