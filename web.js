d5e945a6a9b7dc0b7ecbc82ac2a7e302580d7809
SirBif <bifmadeinsabbioni@gmail.com>
2013-05-12 22:04:51 +0200

    try sql

--- web.js
+++ web.js
@@ -70,12 +70,14 @@ app.get('/doAnUpdate', Facebook.loginRequired({scope : "user_events, friends_eve
 	pool.getConnection(function(err, connection) {
 		if (err) res.end(err);
 		console.log(connection);
+		res.end(connection);
+		/*
 		connection.query( 'SELECT 1 + 1 AS solution from dual', function(err, rows) {
 			connection.end();
 			if (err) throw err;
 			res.end('The solution is: ', rows[0].solution);			
 		});
-	});
+	});*/
 });
 /*
 {
