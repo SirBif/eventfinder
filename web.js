7ede00d38c2924245be16aa3bbc039c9c7e4d1bf
SirBif <bifmadeinsabbioni@gmail.com>
2013-05-12 12:35:45 +0200

    prova

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'sdjdkssdm8sdf89fmdf8sdfmsd' }));
-  app.use(Facebook.middleware({ appId: '180798855409162', secret: '7d5b6a35d8c08d6aac512b6d85243a2f' }));
+  app.use(Facebook.middleware({ appId: process.env.FACEBOOK_APP_ID, secret: process.env.FACEBOOK_SECRET }));
 });
 
 var port = process.env.PORT || 3000;
@@ -17,8 +17,18 @@ app.listen(port, function() {
 });
 
 app.get('/', Facebook.loginRequired(), function (req, res) {
-  req.facebook.api('/me', function(err, user) {
+  req.facebook.api("/fql?q=" + escape("SELECT eid, start_time FROM event WHERE privacy='OPEN' AND start_time > now() AND eid IN (SELECT eid FROM event_member WHERE start_time > now() AND (uid IN(SELECT uid2 FROM friend WHERE uid1=me()) OR uid=me())ORDER BY start_time ASC LIMIT 50) ORDER BY start_time ASC ")
+  , function(err, result) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, ' + user.name + '!');
+	console.log(result);
+    res.end(result);
   });
 });
+
+/*
+{
+"theevent":"select eid, attending_count, unsure_count, location, venue.id, start_time, privacy, end_time from event where eid='373581432761001'",
+"thevenue":"select location.latitude, location.longitude from page where page_id in (select venue.id from #theevent )",
+"attendinggirls":"select uid, sex from user where sex = 'female' and uid in (select uid from event_member where eid in (select eid from #theevent) and rsvp_status = 'attending')"
+}
+*/
